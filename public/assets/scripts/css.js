8ea8ece7ba1491c9f7cbd8faa34dd77b8080ba72
Cedrus32 <69023504+Cedrus32@users.noreply.github.com>
2025-05-23 15:03:16 -0500

    Dynamically size images down to min view width of 1000px

--- public/assets/scripts/css.js
+++ public/assets/scripts/css.js
@@ -2,5 +2,5 @@ let buttonsHeight = document.querySelector(".buttons").offsetHeight;
 let footerHeight = document.querySelector("footer").offsetHeight;
 let content = document.querySelector(".content");
 
-let contentHeight = `calc(100vh - calc(2 * var(--tb-margin)) - var(--layout-gap) - ${buttonsHeight}px - ${footerHeight}px)`;
+let contentHeight = `calc(100vh - calc(2 * var(--tb-margin)) - calc(2 * var(--layout-gap)) - ${buttonsHeight}px - ${footerHeight}px)`;
 content.style.height = contentHeight;
