287972b2d96418bb41e7f311e4b25db3379c83d4
Cedrus32 <69023504+Cedrus32@users.noreply.github.com>
2025-05-26 15:58:53 -0500

    Resize content on window resize -- eliminate the need to refresh to accurately render content sizing.

--- public/assets/scripts/size_content.js
+++ public/assets/scripts/size_content.js
@@ -1,16 +1,17 @@
-// add event listener for button or footer height change
-// add event listener for body width cross 100px threshold
-
 let content = document.querySelector(".content");
-let buttonsHeight = document.querySelector(".buttons").offsetHeight;
 
-let contentHeight;
-if (window.innerWidth < 1000) {
-	let navHeight = document.querySelector(".global_links").offsetHeight;
-	contentHeight = `calc(100dvh - calc(4 * var(--layout-gap)) - ${buttonsHeight}px - ${navHeight}px)`;
-} else {
-	let footerHeight = document.querySelector("footer").offsetHeight;
-	contentHeight = `calc(100dvh - calc(2 * var(--tb-margin)) - calc(2 * var(--layout-gap)) - ${buttonsHeight}px - ${footerHeight}px)`;
+function setSize() {
+	let buttonsHeight = document.querySelector(".buttons").offsetHeight;
+	let contentHeight;
+	if (window.innerWidth < 1000) {
+		let navHeight = document.querySelector(".global_links").offsetHeight;
+		contentHeight = `calc(100dvh - calc(4 * var(--layout-gap)) - ${buttonsHeight}px - ${navHeight}px)`;
+	} else {
+		let footerHeight = document.querySelector("footer").offsetHeight;
+		contentHeight = `calc(100dvh - calc(2 * var(--tb-margin)) - calc(2 * var(--layout-gap)) - ${buttonsHeight}px - ${footerHeight}px)`;
+	}
+	content.style.height = contentHeight;
 }
 
-content.style.height = contentHeight;
+setSize();
+window.onresize = setSize;
