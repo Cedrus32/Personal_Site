825b8273cc657aa0347ca435894c481f02f2e73a
Cedrus32 <69023504+Cedrus32@users.noreply.github.com>
2025-05-26 08:20:49 -0500

    Fix content height styling rules

--- assets/scripts/size_content.js
+++ assets/scripts/size_content.js
@@ -1,6 +1,16 @@
-let buttonsHeight = document.querySelector(".buttons").offsetHeight;
-let footerHeight = document.querySelector("footer").offsetHeight;
-let content = document.querySelector(".content");
+// add event listener for button or footer height change
+// add event listener for body width cross 100px threshold
 
-let contentHeight = `calc(100vh - calc(2 * var(--tb-margin)) - calc(2 * var(--layout-gap)) - ${buttonsHeight}px - ${footerHeight}px)`;
+let content = document.querySelector(".content");
+let buttonsHeight = document.querySelector(".buttons").offsetHeight;
+
+let contentHeight;
+if (window.innerWidth < 1000) {
+	let navHeight = document.querySelector(".global_links").offsetHeight;
+	contentHeight = `calc(100dvh - calc(4 * var(--layout-gap)) - ${buttonsHeight}px - ${navHeight}px)`;
+} else {
+	let footerHeight = document.querySelector("footer").offsetHeight;
+	contentHeight = `calc(100dvh - calc(2 * var(--tb-margin)) - calc(2 * var(--layout-gap)) - ${buttonsHeight}px - ${footerHeight}px)`;
+}
+
 content.style.height = contentHeight;
