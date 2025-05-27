a175a1256e3f76af5c002c9686c3d61f334d0c72
Cedrus32 <69023504+Cedrus32@users.noreply.github.com>
2025-05-27 05:26:58 -0500

    Implement theme switcher. Currently reloads after every page change. Need to optimize full site.

--- public/assets/scripts/size_content.js
+++ public/assets/scripts/size_content.js
@@ -1,13 +1,13 @@
 let content = document.querySelector(".content");
 
 function setSize() {
-	let buttonsHeight = document.querySelector(".buttons").offsetHeight;
 	let contentHeight;
+	const buttonsHeight = document.querySelector(".buttons").offsetHeight;
 	if (window.innerWidth < 1000) {
-		let navHeight = document.querySelector(".global_links").offsetHeight;
+		const navHeight = document.querySelector(".global_links").offsetHeight;
 		contentHeight = `calc(100dvh - calc(4 * var(--layout-gap)) - ${buttonsHeight}px - ${navHeight}px)`;
 	} else {
-		let footerHeight = document.querySelector("footer").offsetHeight;
+		const footerHeight = document.querySelector("footer").offsetHeight;
 		contentHeight = `calc(100dvh - calc(2 * var(--tb-margin)) - calc(2 * var(--layout-gap)) - ${buttonsHeight}px - ${footerHeight}px)`;
 	}
 	content.style.height = contentHeight;
