15516b9612546e50f5bccac2c7d4963b53db1ade
Cedrus32 <69023504+Cedrus32@users.noreply.github.com>
2025-07-14 16:40:54 -0500

    Fix issues brought up by WAVE evaluation tool

--- src/assets/scripts/applyInitText.js
+++ src/assets/scripts/applyInitText.js
@@ -1,14 +1,14 @@
-function applyInitText(element) {
-	const isClosed = element.parentElement.hasAttribute("open");
-	if (!isClosed) {
-		let styleSwitch = element.parentElement.children[1].children[0].children[0];
-		let animateSwitch = element.parentElement.children[1].children[1].children[0];
-		let fontSwitch = element.parentElement.children[1].children[2].children[0].children[1].children;
-		const style = localStorage.getItem("style");
-		const animate = localStorage.getItem("animate");
-		const font = localStorage.getItem("font");
-		styleSwitch.textContent = "theme: " + style;
-		animateSwitch.textContent = "animate: " + animate;
-		fontSwitch.namedItem(font).selected = true;
-	}
+function applyInitText() {
+	let root = document.getElementById("options-close");
+	let styleSwitch = root.parentElement.children[1].children[0].children[0];
+	let animateSwitch = root.parentElement.children[1].children[1].children[0];
+	let fontSwitch = root.parentElement.children[1].children[2].children[0].children[1].children;
+	const style = localStorage.getItem("style");
+	const animate = localStorage.getItem("animate");
+	const font = localStorage.getItem("font");
+	styleSwitch.textContent = "theme: " + style;
+	animateSwitch.textContent = "animate: " + animate;
+	fontSwitch.namedItem(font).selected = true;
 }
+
+applyInitText();
