3a234bce45d15e623eb58490322ec4742e102eb5
Cedrus32 <69023504+Cedrus32@users.noreply.github.com>
2025-10-30 05:31:50 -0500

    Update runs and growth charts.

--- public/assets/scripts/openDialog.js
+++ public/assets/scripts/openDialog.js
@@ -1,11 +1,9 @@
 function openDialog(element) {
-	console.log(element.nextElementSibling);
 	const dialog = element.nextElementSibling;
 	dialog.showModal();
 }
 
 function closeDialog(element) {
-	console.log(element.parentElement);
 	const dialog = element.parentElement;
 	dialog.close();
 }
